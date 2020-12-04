37e021bf8b154812985b124f6782e1637241f54e
mehran1421 <m.kamrani1421@gmail.com>
2020-12-04 19:34:10 +0330

    add Toolbar with vuetify

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <v-toolbar dark>
+      <v-toolbar-title class=" text-uppercase">my todo app</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <span right class="grey--text">SIGN OUT</span>
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
