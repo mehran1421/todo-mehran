ca8b4e7ab4aae02a9cc7267750173f32b10a76c1
mehran1421 <m.kamrani1421@gmail.com>
2020-12-04 22:18:00 +0330

    add router and dashboard in side bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,58 @@
 <template>
-    <nav>
-    <v-toolbar dark app src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-     <v-app-bar-nav-icon class="grey--text" @click="drawer=!drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title class=" text-uppercase">my todo app</v-toolbar-title>
+  <nav>
+    <v-toolbar
+      dark
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon
+        class="font-weight-bold"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase">
+        <span class="font-weight-bold">my todo app</span>
+      </v-toolbar-title>
 
       <v-spacer></v-spacer>
-      <span right class="grey--text">SIGN OUT</span>
+      <span right class="white--text font-weight-black">SIGN OUT</span>
       <v-btn icon>
-        <v-icon>mdi-export</v-icon>
+        <v-icon class="font-weight-bold">mdi-export</v-icon>
       </v-btn>
     </v-toolbar>
-    <v-navigation-drawer app v-model="drawer" temporary absolute class="indigo">
-    <p>hello</p>
+    <v-navigation-drawer v-model="drawer" temporary absolute class="primary">
+      <v-list nav dense>
+        <v-list-item-group v-model="selectedItem" color="white">
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            router
+            :to="item.rout"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            drawer:false,
-        }
-    }
-    
-}
+  data() {
+    return {
+      drawer: false,
+      selectedItem: 0,
+      items: [
+        { text: "Dashboard", icon: "mdi-view-dashboard", rout: "/" },
+        { text: "my project", icon: "mdi-folder", rout: "/project" },
+        { text: "team", icon: "mdi-account-multiple", rout: "/team" },
+      ],
+    };
+  },
+};
 </script>
