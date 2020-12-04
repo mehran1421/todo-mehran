0c92c6f888c67633290f970521c22d348c181be8
mehran1421 <m.kamrani1421@gmail.com>
2020-12-04 19:55:14 +0330

    add navigations

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-    <v-toolbar dark>
+    <nav>
+    <v-toolbar dark app>
+     <v-app-bar-nav-icon class="grey--text" @click="drawer=!drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class=" text-uppercase">my todo app</v-toolbar-title>
 
       <v-spacer></v-spacer>
@@ -9,11 +10,26 @@
         <v-icon>mdi-export</v-icon>
       </v-btn>
     </v-toolbar>
-  </div>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      absolute
+      bottom
+      temporary
+      class="indigo"
+    >
+    <p>hello</p>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            drawer:false,
+        }
+    }
     
 }
 </script>
