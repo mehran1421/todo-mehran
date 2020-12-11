7758a2cb31cfba3d391f0757038d3317033716a6
mehran1421 <m.kamrani1421@gmail.com>
2020-12-11 16:33:58 +0330

    add grid system

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="dashboard">
-    <v-container>
-      <v-subheader class="grey--text">dashboard</v-subheader>
+  <div class="dashboard grey">
+    <v-row justify="center">
+      <h1 class="subheading white--text align-center">dashboard</h1>
+    </v-row>
+    <v-container class="my-5">
+      <v-row class="white">
+        <v-col cols="12" sm="12" md="6">
+          <div class="caption grey--text">Project title</div>
+          <div>Create a new website</div>
+        </v-col>
+        <v-col cols="12" sm="12" md="2">
+          <div class="caption grey--text">Project title</div>
+          <div>Mehran Kamrani</div>
+        </v-col>
+        <v-col cols="12" sm="12" md="2">
+          <div class="caption grey--text">Due by</div>
+          <div>21/9/1399</div>
+        </v-col>
+        <v-col cols="12" sm="12" md="2">
+          <div class="caption grey--text">Person</div>
+          <div>Mehran Kamrani</div>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
